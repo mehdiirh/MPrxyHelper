9a148cc11ae311ac7636eb6a8d8c7f4c15ec2d27
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-09-29 14:31:45 +0330

    [BUGFIX] error message of there are no admins

--- main.py
+++ main.py
@@ -291,7 +291,7 @@ async def get_mods(message: Message):
 
     admins = get_admins()
 
-    text = "" if admins else "No identified."
+    text = "" if admins else "No admins identified."
     for idx, admin in enumerate(admins):
         text += f"{idx + 1} - [ `{admin}` ]\n"
 
